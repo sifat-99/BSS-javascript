83d6f3e00bb65e22e5ea3d4958ad27f41dfdd0dc
sifat-99 <mdabdurrahmansifat@gmail.com>
2026-06-02 12:47:00 +0600

    dynamic typing practice complete

---
+++ Basics/dynamic_typing.js
@@ -0,0 +1,11 @@
+let value = "Sifat";
+console.log(typeof value);
+
+value = 25;
+console.log(typeof value);
+
+value = true;
+console.log(typeof value);
+
+value = null;
+console.log(typeof value);
