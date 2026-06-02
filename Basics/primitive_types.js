5aa66087c85c22137e0c3191fb42e4c241d7fc2b
sifat-99 <mdabdurrahmansifat@gmail.com>
2026-06-02 12:22:00 +0600

    primitive types practice complete

---
+++ Basics/primitive_types.js
@@ -0,0 +1,11 @@
+let name = "Sifat";
+let age = 25;
+let isDeveloper = true;
+let firstName = undefined;
+let selectedColor = null;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isDeveloper);
+console.log(typeof firstName);
+console.log(typeof selectedColor);
