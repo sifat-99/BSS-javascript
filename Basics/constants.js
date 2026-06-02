387cfee1fe178eb92047d97709b74b6f5cb39c8d
sifat-99 <mdabdurrahmansifat@gmail.com>
2026-06-02 12:10:00 +0600

    constant practice complete

---
+++ Basics/constants.js
@@ -0,0 +1,11 @@
+const PI = 3.14159;
+console.log("PI:", PI);
+
+const birthYear = 2000;
+console.log("Birth Year:", birthYear);
+
+const name = "Sifat";
+console.log("Name:", name);
+
+const interestRate = 0.3;
+console.log(interestRate);
