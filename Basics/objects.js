c3f337c1ed97bf9b2c7a0071b969a506f6c1f9ee
sifat-99 <mdabdurrahmansifat@gmail.com>
2026-06-02 13:05:00 +0600

    object practice complete

---
+++ Basics/objects.js
@@ -0,0 +1,12 @@
+let person = {
+    name: "Sifat",
+    age: 25
+};
+
+console.log(person);
+
+person.name = "Sifat_Dev";
+console.log(person.name);
+
+person["age"] = 26;
+console.log(person["age"]);
