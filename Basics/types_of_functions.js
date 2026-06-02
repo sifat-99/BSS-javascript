11c3f07bcae62cdd90e9430884941c63cfd7a265
sifat-99 <mdabdurrahmansifat@gmail.com>
2026-06-02 14:10:00 +0600

    types of function practice complete

---
+++ Basics/types_of_functions.js
@@ -0,0 +1,11 @@
+function greet(name) {
+    console.log("Hello " + name);
+}
+
+function square(number) {
+    return number * number;
+}
+
+greet("Sifat");
+let result = square(5);
+console.log(result);
