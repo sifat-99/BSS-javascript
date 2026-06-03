f98fcc16ec94e66be3232158629dbfb74057426a
sifat-99 <mdabdurrahmansifat@gmail.com>
2026-06-03 09:05:00 +0600

    arithmetic.js practice complete

---
+++ Operators/arithmetic.js
@@ -0,0 +1,17 @@
+let x = 10;
+let y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
+
+console.log(++x);
+console.log(x++);
+console.log(x);
+
+console.log(--y);
+console.log(y--);
+console.log(y);
